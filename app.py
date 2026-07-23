d625ba02ec420f434b4a2ece063dc9eb833cbff5
Manik2604 <manikjindal35@gmail.com>
2026-07-23 09:48:58 +0530

    Fix formatting in app.py for developer details section

--- app.py
+++ app.py
@@ -234,9 +234,9 @@ Predict the salary of an employee using a Machine Learning model.
                     """
 # 👩‍💻 Developer Details
 
-*Name:* Manik
+**Name:** Manik
 
-*College:*  
+**College:**  
 Panipat Institute of Engineering and Technology
 
 ---
@@ -247,7 +247,7 @@ Salary Prediction using Linear Regression
 
 ---
 
-## 🛠️ Technology Used
+## 🛠 Technology Used
 
 - Python
 - Pandas
